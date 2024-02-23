74a629faf00dce637b945fda8ef814aab12a87b4
Paula <paula.foitzik@student.ie.edu>
2024-02-23 23:36:40 +0100

    add symboldetails via API call to Alphavantage + modify route to view stockoverview per user

--- Milestone0/app.py
+++ Milestone0/app.py
@@ -8,10 +8,10 @@ def homepage():
     return """<h1>Welcome to Debugging Dollars!</h1>
     <p> Please choose which user you are:</p>
     <div>
-    <ul>User1: Click <a href='/stockinfo/user1'>here</a></ul>
+    <ul>User1: Click <a href='/user1'>here</a></ul>
     </div>"""
 
-@app.route("/stockinfo/<userID>")
+@app.route("/<userID>")
 def stock_overview(userID):
     user_data = user_database()
     if userID not in user_data:
@@ -21,9 +21,33 @@ def stock_overview(userID):
     return jsonify(user_info)
 
 
-@app.route('/stockinfo/<userID>/<symbol>')
-def symbol_info(userID,symbol):
-    pass
+@app.route('/stockinfo/<symbol>')
+def symbol_info(symbol):
+    
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey=5KFQLJAEXPPU6DJ9&outputsize=compact&datatype=json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        symbol_data = response.json()
+        # Extract the "Time Series (Daily)" part of the response
+        daily_series = symbol_data.get("Time Series (Daily)", {})
+        
+        # Extract the last 5 days of data
+        last_5_days = list(daily_series.keys())[:5]
+        data_for_frontend = []
+        for date in last_5_days:
+            day_data = daily_series[date]
+            data_for_frontend.append({
+                "date": date,
+                "open": day_data["1. open"],
+                "high": day_data["2. high"],
+                "low": day_data["3. low"],
+                "close": day_data["4. close"],
+                "volume": day_data["5. volume"]
+            })
+    else:
+        print(f"Failed to get data: {response.status_code}")
+
+    return jsonify(data_for_frontend)
 
 
 def user_database():
@@ -34,15 +58,7 @@ def user_database():
             'GOOGL': 5, 
 
             'AMZN': 3 
-        }, 
-
-        'user2': { 
-            'AAPL': 5, 
-
-            'GOOGL': 3, 
-
-            'AMZN': 2 
-        } 
+        }
     } 
 
 if __name__ == '__main__':
